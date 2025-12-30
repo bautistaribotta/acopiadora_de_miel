1f7f82c48664d2d7d74b9ba6cba44fc890a6208b
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-12-30 10:27:37 -0300

    Creo la funcion para crear un nuevo cliente y configuro la conexion para las funciones aun no implementadas de: buscar_cliente y eliminar_cliente

--- model/clientes_db.py
+++ model/clientes_db.py
@@ -1,2 +1,40 @@
 import mysql.connector
 
+def buscar_cliente():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+
+def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    sql = ("""INSERT INTO clientes (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
+           VALUES (%s, %s, %s, %s, %s, %s, %s)""")
+    valores = (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
+
+    cursor.execute(sql, valores)
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+
+def eliminar_cliente():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
