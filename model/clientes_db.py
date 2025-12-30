f7c788fe3c2e9d4f27b4f040626532e8126c4a82
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-12-30 12:28:39 -0300

    Integro las funciones de buscar producto por nombre e id y las funciones de insertar y eliminar

--- model/clientes_db.py
+++ model/clientes_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def buscar_cliente(nombre):
+def buscar_cliente_nombre(nombre_cliente):
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -9,7 +9,34 @@ def buscar_cliente(nombre):
     )
     cursor = conexion.cursor()
 
-    intruccion_sql = f"SELECT (id, nombre, localidad, telefono) FROM clientes WHERE nombre = {nombre}"
+    intruccion_sql = "SELECT id, nombre, localidad, telefono FROM clientes WHERE nombre LIKE %s"
+    valor = (f"%{nombre_cliente}%",)
+    cursor.execute(intruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
+
+
+def buscar_cliente_id(id_cliente):
+    """Tener en cuenta que esta funcion usa LIKE ya que hace una busqueda en tiempo real"""
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    intruccion_sql = f"SELECT id, nombre, localidad, telefono FROM clientes WHERE id LIKE %s"
+    valor = (f"%{id_cliente}%",)
+    cursor.execute(intruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
 
 
 def insertar_cliente(nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
@@ -25,7 +52,6 @@ def insertar_cliente(nombre, apellido, telefono, localidad, direccion, factura_p
                     (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
                     VALUES (%s, %s, %s, %s, %s, %s, %s)"""
     valores = (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
-
     cursor.execute(instruccion_sql, valores)
     conexion.commit()
 
@@ -42,10 +68,9 @@ def eliminar_cliente(id_cliente):
     )
     cursor = conexion.cursor()
 
-    instruccion_sql = "DELETE FROM clientes WHERE ID = %s"
+    instruccion_sql = "DELETE FROM clientes WHERE id = %s"
     # Debe llevar una "," al final para ser tomado como tupla
     valores = (id_cliente,)
-
     cursor.execute(instruccion_sql, valores)
     conexion.commit()
 
--- model/productos_db.py
+++ model/productos_db.py
@@ -1,1 +1,78 @@
 import mysql.connector
+
+
+def buscar_producto_nombre(nombre_producto):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    instruccion_sql = "SELECT id, nombre, categoria, cantidad FROM productos WHERE nombre LIKE %s"
+    valor = (f"%{nombre_producto}%",)
+
+    cursor.execute(instruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
+
+
+def buscar_producto_id(id_producto):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    instruccion_sql = "SELECT id, nombre, categoria, cantidad FROM productos WHERE id LIKE %s"
+    valor = (f"%{id_producto}%",)
+
+    cursor.execute(instruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
+
+
+def insertar_producto(nombre, categoria, unidad_medida, precio, cantidad):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    instruccion_sql = """INSERT INTO productos (nombre, categoria, unidad_medida, precio, cantidad) 
+                        VALUES (%s, %s, %s, %s, %s)"""
+    valores = (nombre, categoria, unidad_medida, precio, cantidad)
+    cursor.execute(instruccion_sql, valores)
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+
+def eliminar_producto(id_producto):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    instruccion_sql = """DELETE FROM productos WHERE id = %s"""
+    valor = (id_producto,)
+    cursor.execute(instruccion_sql, valor)
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
