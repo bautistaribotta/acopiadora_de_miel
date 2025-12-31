f0fe7d00c0e8b06eded907412cb8a83b94fc990b
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-12-31 20:44:08 -0300

    Agregada la clase Cliente, a su vez, aplico el patron de diseño DAO para crear y editar el mismo

--- model/clientes_db.py
+++ model/clientes_db.py
@@ -3,55 +3,16 @@ import mysql.connector
 
 class Cliente:
     def __init__(self, nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
-        self._nombre = nombre
-        self._apellido = apellido
-        self._telefono = telefono
-        self._localidad = localidad
-        self._direccion = direccion
-        self._factura_produccion = factura_produccion
-        self._cuit = cuit
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.localidad = localidad
+        self.direccion = direccion
+        self.factura_produccion = factura_produccion
+        self.cuit = cuit
 
 
-def buscar_cliente_nombre(nombre_cliente):
-    conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="southern_honey_group"
-    )
-    cursor = conexion.cursor()
-
-    intruccion_sql = "SELECT id, nombre, localidad, telefono FROM clientes WHERE nombre LIKE %s"
-    valor = (f"%{nombre_cliente}%",)
-    cursor.execute(intruccion_sql, valor)
-    resultados = cursor.fetchall()
-
-    cursor.close()
-    conexion.close()
-    return resultados
-
-
-def buscar_cliente_id(id_cliente):
-    """Tener en cuenta que esta funcion usa LIKE ya que hace una busqueda en tiempo real"""
-    conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="southern_honey_group"
-    )
-    cursor = conexion.cursor()
-
-    intruccion_sql = f"SELECT id, nombre, localidad, telefono FROM clientes WHERE id LIKE %s"
-    valor = (f"%{id_cliente}%",)
-    cursor.execute(intruccion_sql, valor)
-    resultados = cursor.fetchall()
-
-    cursor.close()
-    conexion.close()
-    return resultados
-
-
-def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
+def nuevo_cliente(cliente : Cliente):
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -63,7 +24,11 @@ def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_prod
     instruccion_sql = """INSERT INTO clientes 
                     (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
                     VALUES (%s, %s, %s, %s, %s, %s, %s)"""
-    valores = (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
+
+    valores = (cliente.nombre, cliente.apellido, cliente.telefono,
+               cliente.localidad, cliente.direccion,
+               cliente.factura_produccion, cliente.cuit)
+
     cursor.execute(instruccion_sql, valores)
     conexion.commit()
 
@@ -71,7 +36,7 @@ def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_prod
     conexion.close()
 
 
-def editar_cliente(id_cliente, nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
+def editar_cliente(id_cliente, cliente : Cliente):
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -82,7 +47,10 @@ def editar_cliente(id_cliente, nombre, apellido, telefono, localidad, direccion,
 
     intruccion_sql = ("UPDATE clientes SET nombre=%s, apellido=%s, telefono=%s, "
                       "localidad=%s, direccion=%s, factura_produccion=%s, cuit=%s WHERE id = %s")
-    valores = (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit, id_cliente)
+
+    valores = (cliente.nombre, cliente.apellido,
+               cliente.telefono, cliente.localidad, cliente.direccion,
+               cliente.factura_produccion, cliente.cuit, id_cliente)
     cursor.execute(intruccion_sql, valores)
 
     conexion.commit()
@@ -107,3 +75,42 @@ def eliminar_cliente(id_cliente):
 
     cursor.close()
     conexion.close()
+
+
+def buscar_cliente_id(id_cliente):
+    """Tener en cuenta que esta funcion usa LIKE ya que hace una busqueda en tiempo real"""
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    intruccion_sql = f"SELECT id, nombre, localidad, telefono FROM clientes WHERE id LIKE %s"
+    valor = (f"%{id_cliente}%",)
+    cursor.execute(intruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
+
+
+def buscar_cliente_nombre(nombre_cliente):
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="southern_honey_group"
+    )
+    cursor = conexion.cursor()
+
+    intruccion_sql = "SELECT id, nombre, localidad, telefono FROM clientes WHERE nombre LIKE %s"
+    valor = (f"%{nombre_cliente}%",)
+    cursor.execute(intruccion_sql, valor)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultados
