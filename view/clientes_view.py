d2f2f54215feb8529771808bb0d94eafe357a7ec
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-01-09 22:09:44 -0300

    Quite la importacion de OS Operative System en clientes

--- view/clientes_view.py
+++ view/clientes_view.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from operaciones_view import *
 from estilos import *
 from controller.validaciones import *
