e16b4f8f7b1f4d799f46df13ed5ec2a618ff94e3
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-01-14 09:59:36 -0300

    Creada la tabla de deudores y su respectivo frame

--- view/deudores_view.py
+++ view/deudores_view.py
@@ -13,6 +13,27 @@ def listado_deudores():
     ventana_listado_deudores.mainloop()
 
 
+    # Frame tabla
+    frame_tabla_deudores = tk.Frame(ventana_listado_deudores)
+    frame_tabla_deudores.configure()
+    frame_tabla_deudores.pack()
+
+    # Treeview (Tabla)
+    columnas = ("id", "nombre", "debe")
+    tabla_deudores = ttk.Treeview(frame_tabla_deudores, columns=columnas, show="headings")
+
+    tabla_deudores.heading("id", text="ID")
+    tabla_deudores.heading("nombre", text="Nombre")
+    tabla_deudores.heading("debe", text="Total adeudado")
+
+    tabla_deudores.column("id", width=80, anchor="center")
+    tabla_deudores.column("nombre", width=250, anchor="w")
+    tabla_deudores.column("debe", width=300, anchor="center")
+
+    tabla_deudores.pack()
+
+
+
 if __name__ == "__main__":
     ventana = tk.Tk()
     ventana.withdraw()
