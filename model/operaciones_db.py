ea1bdd19936e3002570b74e68ee95e592dcd52ee
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-01-26 12:53:56 -0300

    Agrego la funcion para crear una nueva operacion

--- model/operaciones_db.py
+++ model/operaciones_db.py
@@ -1,12 +1,8 @@
 from model.conexion_db import abrir_conexion
+from model.entidades import Operacion, DetalleOperacion
 
 
 def buscar_operaciones_cliente(id_cliente):
-    """
-    Esta funcion permite traer todo el listado de operaciones de un cliente
-    :param id_cliente:
-    :return listado de operaciones del cliente:
-    """
     with abrir_conexion() as (cursor, conexion):
         intruccion_sql = "SELECT * FROM operaciones WHERE id_cliente = %s"
         cursor.execute(intruccion_sql, id_cliente)
@@ -14,4 +10,33 @@ def buscar_operaciones_cliente(id_cliente):
         return resultados
 
 
-def nueva_operacion(id_cliente):
+def nueva_operacion(operacion: Operacion, lista_detalles: list):
+    with abrir_conexion() as (cursor, conexion):
+        sql_operacion = """
+            INSERT INTO operaciones 
+            (id_cliente, monto_total, valor_dolar, valor_kilo_miel) 
+            VALUES (%s, %s, %s, %s)
+        """
+        valores_operacion = (
+            operacion.id_cliente,
+            operacion.monto_total,
+            operacion.valor_dolar,
+            operacion.valor_kilo_miel
+        )
+        cursor.execute(sql_operacion, valores_operacion)
+
+        # 2. Recuperar ID
+        id_nueva_operacion = cursor.lastrowid
+
+        # 3. Insertar detalles
+        sql_detalle = """
+            INSERT INTO detalle_operaciones (id_operacion, id_producto, cantidad) 
+            VALUES (%s, %s, %s)
+        """
+
+        datos_detalles = []
+        for detalle in lista_detalles:
+            datos_detalles.append((id_nueva_operacion, detalle.id_producto, detalle.cantidad))
+
+        cursor.executemany(sql_detalle, datos_detalles)
+        conexion.commit()
